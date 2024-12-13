7d6653b180fa4e5ca36dc6098d8e76bf09f92da5
BeaverySoup <82706334+ItzLeirb@users.noreply.github.com>
2024-12-13 11:45:31 +0100

    test for affichage

--- tests.py
+++ tests.py
@@ -1,2 +1,15 @@
 from game import *
-from main import *
+from main import afficherGrilleConsole
+
+
+grille = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 2, 0, 0, 0],
+    [0, 0, 1, 2, 1, 0, 0, 0],
+    [0, 1, 2, 1, 2, 0, 0, 0],
+]
+
+afficherGrilleConsole(grille)
