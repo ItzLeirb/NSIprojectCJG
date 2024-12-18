d01f454c2934a6f0cf3d33264a02fdf1915f735e
BeaverySoup <82706334+ItzLeirb@users.noreply.github.com>
2024-12-18 11:34:57 +0100

    tests for isdBLTR

--- tests.py
+++ tests.py
@@ -2,38 +2,71 @@ from game import *
 from main import affichageConsole
 
 
-grille = [
-    [0, 0, 0, 0, 0, 2],
-    [0, 0, 0, 0, 2, 1],
-    [0, 0, 0, 2, 1, 2],
-    [0, 0, 2, 1, 2, 1],
-    [0, 0, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0]
-]
+# test affichage
 
+grille = [
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1],
+    [0, 0, 2, 1, 2, 1],
+    [0, 0, 0, 2, 1, 2],
+    [0, 0, 0, 0, 2, 1],
+    [0, 0, 0, 0, 0, 2]
+]
 affichageConsole(grille, 1)
+
+# test isDiagonalBottomLeftToTopRightWin
 
 assert isDiagonalBottomLeftToTopRightWin(grille, 3, 2, 2)==True
 grille = [
-    [0, 0, 0, 0, 0, 2],
-    [0, 0, 0, 0, 2, 1],
-    [0, 0, 0, 1, 1, 2],
-    [0, 0, 2, 1, 2, 1],
-    [0, 0, 0, 0, 0, 1],
     [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1],
+    [0, 0, 2, 1, 2, 1],
+    [0, 0, 0, 1, 1, 2],
+    [0, 0, 0, 0, 2, 1],
+    [0, 0, 0, 0, 0, 2]
 ]
 assert isDiagonalBottomLeftToTopRightWin(grille, 3, 2, 2)==False
 grille = [
-    [0, 0, 0, 0, 0, 2],
-    [0, 0, 0, 0, 1, 1],
-    [0, 0, 0, 2, 1, 2],
-    [0, 0, 2, 1, 2, 1],
-    [0, 2, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0],
     [2, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0]
+    [0, 2, 0, 0, 0, 1],
+    [0, 0, 2, 1, 2, 1],
+    [0, 0, 0, 2, 1, 2],
+    [0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 0, 0, 2]
 ]
-assert isDiagonalBottomLeftToTopRightWin(grille, 2, 3, 2)==True
+assert isDiagonalBottomLeftToTopRightWin(grille, 4, 3, 2)==True
+grille = [
+    [0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 1],
+    [0, 0, 1, 1, 2, 1],
+    [0, 0, 0, 1, 1, 2],
+    [0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 0, 0, 2]
+]
+assert isDiagonalBottomLeftToTopRightWin(grille, 4, 3, 1)==True
+grille = [
+    [0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0],
+    [0, 2, 0, 0, 0, 1],
+    [0, 0, 1, 1, 2, 1],
+    [0, 0, 0, 1, 1, 2],
+    [0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 0, 0, 1]
+]
+assert isDiagonalBottomLeftToTopRightWin(grille, 4, 3, 1)==True
+grille = [
+    [0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0],
+    [0, 2, 0, 0, 0, 1],
+    [0, 0, 2, 1, 2, 1],
+    [0, 0, 0, 1, 1, 2],
+    [0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 0, 0, 1]
+]
+assert isDiagonalBottomLeftToTopRightWin(grille, 4, 3, 1)==True
 
 
