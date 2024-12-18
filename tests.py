6af1ddb22f584bf51efa13d5a159b11e3386959c
BeaverySoup <82706334+ItzLeirb@users.noreply.github.com>
2024-12-18 10:50:05 +0000

    tests for isdbltr

--- tests.py
+++ tests.py
@@ -5,13 +5,13 @@ from main import affichageConsole
 # test affichage
 
 grille = [
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1],
-    [0, 0, 2, 1, 2, 1],
-    [0, 0, 0, 2, 1, 2],
+    [0, 0, 0, 0, 0, 2],
     [0, 0, 0, 0, 2, 1],
-    [0, 0, 0, 0, 0, 2]
+    [0, 0, 0, 2, 1, 2],
+    [0, 0, 2, 1, 2, 1],
+    [0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
 ]
 affichageConsole(grille, 1)
 
@@ -19,54 +19,64 @@ affichageConsole(grille, 1)
 
 assert isDiagonalBottomLeftToTopRightWin(grille, 3, 2, 2)==True
 grille = [
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1],
-    [0, 0, 2, 1, 2, 1],
-    [0, 0, 0, 1, 1, 2],
+    [0, 0, 0, 0, 0, 2],
     [0, 0, 0, 0, 2, 1],
-    [0, 0, 0, 0, 0, 2]
+    [0, 0, 0, 1, 1, 2],
+    [0, 0, 2, 1, 2, 1],
+    [0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
 ]
 assert isDiagonalBottomLeftToTopRightWin(grille, 3, 2, 2)==False
 grille = [
-    [0, 0, 0, 0, 0, 0],
-    [2, 0, 0, 0, 0, 0],
-    [0, 2, 0, 0, 0, 1],
-    [0, 0, 2, 1, 2, 1],
+    [0, 0, 0, 0, 0, 2],
+    [0, 0, 0, 0, 1, 1],
     [0, 0, 0, 2, 1, 2],
-    [0, 0, 0, 0, 1, 1],
-    [0, 0, 0, 0, 0, 2]
-]
-assert isDiagonalBottomLeftToTopRightWin(grille, 4, 3, 2)==True
-grille = [
-    [0, 0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 1],
-    [0, 0, 1, 1, 2, 1],
-    [0, 0, 0, 1, 1, 2],
-    [0, 0, 0, 0, 1, 1],
-    [0, 0, 0, 0, 0, 2]
-]
-assert isDiagonalBottomLeftToTopRightWin(grille, 4, 3, 1)==True
-grille = [
-    [0, 0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 0, 0],
-    [0, 2, 0, 0, 0, 1],
-    [0, 0, 1, 1, 2, 1],
-    [0, 0, 0, 1, 1, 2],
-    [0, 0, 0, 0, 1, 1],
-    [0, 0, 0, 0, 0, 1]
-]
-assert isDiagonalBottomLeftToTopRightWin(grille, 4, 3, 1)==True
-grille = [
-    [0, 0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 0, 0],
-    [0, 2, 0, 0, 0, 1],
     [0, 0, 2, 1, 2, 1],
-    [0, 0, 0, 1, 1, 2],
-    [0, 0, 0, 0, 1, 1],
-    [0, 0, 0, 0, 0, 1]
+    [0, 2, 0, 0, 0, 1],
+    [2, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
 ]
-assert isDiagonalBottomLeftToTopRightWin(grille, 4, 3, 1)==True
+assert isDiagonalBottomLeftToTopRightWin(grille, 2, 3, 2)==True
+grille = [
+    [0, 0, 0, 0, 0, 2],
+    [0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 1, 1, 2],
+    [0, 0, 1, 1, 2, 1],
+    [0, 1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+]
+assert isDiagonalBottomLeftToTopRightWin(grille, 2, 3, 1)==True
+grille = [
+    [0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 1, 1, 2],
+    [0, 0, 1, 1, 2, 1],
+    [0, 2, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+]
+assert isDiagonalBottomLeftToTopRightWin(grille, 2, 3, 1)==True
+grille = [
+    [0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 1, 1, 2],
+    [0, 0, 2, 1, 2, 1],
+    [0, 2, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+]
+assert isDiagonalBottomLeftToTopRightWin(grille, 2, 3, 1)==True
+grille = [
+    [0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 1, 1, 2],
+    [0, 0, 2, 1, 2, 1],
+    [0, 2, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+]
+assert isDiagonalBottomLeftToTopRightWin(grille, 2, 3, 1)==True
 
 
