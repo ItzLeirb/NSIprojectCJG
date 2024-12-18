1e6c55691504623d4a86737e7e82fecc4b014491
BeaverySoup <82706334+ItzLeirb@users.noreply.github.com>
2024-12-18 11:23:24 +0100

    tests

--- tests.py
+++ tests.py
@@ -15,3 +15,23 @@ grille = [
 affichageConsole(grille, 1)
 
 assert isDiagonalBottomLeftToTopRightWin(grille, 3, 2, 2)==True
+grille = [
+    [0, 0, 0, 0, 0, 2],
+    [0, 0, 0, 0, 2, 1],
+    [0, 0, 0, 1, 1, 2],
+    [0, 0, 2, 1, 2, 1],
+    [0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+]
+assert isDiagonalBottomLeftToTopRightWin(grille, 3, 2, 2)==False
+grille = [
+    [0, 0, 0, 0, 0, 2],
+    [0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 2, 1, 2],
+    [0, 0, 2, 1, 2, 1],
+    [0, 2, 0, 0, 0, 1],
+    [2, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+]
+assert isDiagonalBottomLeftToTopRightWin(grille, 2, 3, 2)==True
