9d6d0b76c7c4dc5a1449f3ad385ecfbbc307348d
Glaccier <jules.graffan@gmail.com>
2024-12-20 11:42:39 +0100

    a

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from settings import *
 
 # Gabriel tests ok
 def affichageConsole(grille: list[list[int]], joueur: int):
+      
     print("  | 0 | 1 | 2 | 3 | 4 | 5 | 6 | ")
     for ligne in range(len(grille[0])):
         ligne_texte = f"{ligne} | "
