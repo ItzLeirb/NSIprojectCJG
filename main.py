8f2c9874df4db43c71eddafce3f79cbaa2b889a2
BeaverySoup <82706334+ItzLeirb@users.noreply.github.com>
2024-12-20 10:49:05 +0000

    Docs / comments for jeu()

--- main.py
+++ main.py
@@ -15,24 +15,35 @@ def affichageConsole(grille: list[list[int]], joueur: int):
     print(f"Au tour du joueur {joueur % 2 + 1}")
     
 
-# Cyprien le best
+# Cyprien
 def jeu():
     """
-    Fait tourner la partie entiere
+    Fait tourner la partie entière.
+    Coordonne les différentes fonctions pour faire fonctioner le jeu.
     """
+    # Création de la grille, mise en place des variables importantes
     grille = [[0]*6]*7
     joueurs = setupJoueur()
     running = True
     joueur = 0
+    nombre_coups = 0
+    
+    # La boucle principale
     while running == True:
+        # Change le joueur
         joueur += 1
         if joueur > 2:
             joueur = 1
 
+        # Fait jouer le joueur
         index_colonne = trouverColonne(joueurs[joueur])
         grille, index_ligne = ajouterJeton(grille, index_colonne, joueur)
-    
+
+        # Vérifie si la partie est finie (victoire ou match nul)
+        nombre_coups += 1
         if isVerticalWin(grille, index_colonne, index_ligne, joueur) == True or isHorizontalWin(grille, index_ligne, joueur) == True or isDiagonalBottomLeftToTopRightWin(grille, index_colonne, index_ligne, joueur) == True or isDiagonalTopLeftToBottomRightWin(grille, index_colonne, index_ligne, joueur) == True: 
+            running == False
+        elif nombre_coups >= 42:
             running == False
 
 
