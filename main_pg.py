1c64ab4e642e527ce4aea606fcd01876433f58c6
BeaverySoup <gabriel.terrenoir@gmail.com>
2025-01-12 08:59:17 +0100

    main_render

--- main_pg.py
+++ main_pg.py
@@ -4,20 +4,26 @@ from os import path
 from game import *
 from settings import *
 
-# pygame setup
-pg.init()
-screen = pg.display.set_mode((1280, 720))
-clock = pg.time.Clock()
-running = True
-
 # constantes pour pygame
 MAIN_DIR = path.dirname('__main__')
 IMG_DIR = path.join(MAIN_DIR, 'img')
 
+SIZE_MULITIPLIER = 6
+WIDTH = 128 * SIZE_MULITIPLIER
+HEIGHT = 128 * SIZE_MULITIPLIER
+SCREEN_SIZE = (WIDTH, HEIGHT)
+
 # chargement des images
 jeton_jaune = pg.image.load(path.join(IMG_DIR, 'jaune.png')) 
 jeton_rouge = pg.image.load(path.join(IMG_DIR, 'rouge.png')) 
-grille_image = pg.image.load(path.join(IMG_DIR, 'grille.png'))
+grille_image = pg.transform.scale(pg.image.load(path.join(IMG_DIR, 'grille.png')), SCREEN_SIZE)
+
+
+# pygame setup
+pg.init()
+screen = pg.display.set_mode(SCREEN_SIZE)
+clock = pg.time.Clock()
+running = True
 
 while running:
     # poll for events
@@ -27,7 +33,7 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.blit(grille_image, (0,0))
 
     # RENDER YOUR GAME HERE
 
