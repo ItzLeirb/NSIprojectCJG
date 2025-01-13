5540121822926bc7c9e4482224fd0f8d8e400db1
Leirb <82706334+ItzLeirb@users.noreply.github.com>
2025-01-13 18:31:54 +0100

    a

--- main_pg.py
+++ main_pg.py
@@ -4,20 +4,24 @@ from os import path
 from game import *
 from settings import *
 
-# pygame setup
-pg.init()
-screen = pg.display.set_mode((1280, 720))
-clock = pg.time.Clock()
-running = True
-
 # constantes pour pygame
 MAIN_DIR = path.dirname('__main__')
 IMG_DIR = path.join(MAIN_DIR, 'img')
 
+SIZE_MULTIPLIER = 2
+WIDTH = 128 * SIZE_MULTIPLIER
+HEIGHT = 128 * SIZE_MULTIPLIER
+
+# pygame setup
+pg.init()
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+clock = pg.time.Clock()
+running = True
+
 # chargement des images
-jeton_jaune = pg.image.load(path.join(IMG_DIR, 'jaune.png')) 
-jeton_rouge = pg.image.load(path.join(IMG_DIR, 'rouge.png')) 
-grille_image = pg.image.load(path.join(IMG_DIR, 'grille.png'))
+jeton_jaune = pg.image.load(path.join(IMG_DIR, 'jaune.png')).convert() 
+jeton_rouge = pg.image.load(path.join(IMG_DIR, 'rouge.png')).convert() 
+grille_image = pg.image.load(path.join(IMG_DIR, 'grille.png')).convert()
 
 while running:
     # poll for events
@@ -26,10 +30,8 @@ while running:
         if event.type == pg.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-
     # RENDER YOUR GAME HERE
+    screen.blit(grille_image, (0,0))
 
     # flip() the display to put your work on screen
     pg.display.flip()
